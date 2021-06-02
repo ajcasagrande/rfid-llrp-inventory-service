89e8e6715ec743bf369199da8e84d3b0c9fd0106
Anthony Casagrande <anthony.j.casagrande@intel.com>
2021-06-02 15:50:59 -0700

    refactor(app): add basic main.go file
    Signed-off-by: Anthony Casagrande <anthony.j.casagrande@intel.com>

---
+++ main.go
@@ -0,0 +1,24 @@
+//
+// Copyright (C) 2021 Intel Corporation
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package main
+
+import (
+	"edgexfoundry-holding/rfid-llrp-inventory-service/internal/inventory/app"
+	"fmt"
+	"os"
+)
+
+func main() {
+	app := inventoryapp.NewInventoryApp()
+	if err := app.Initialize(); err != nil {
+		fmt.Printf("App initialization failed: %v\n", err)
+		os.Exit(1)
+	}
+	if err := app.RunUntilCancelled(); err != nil {
+		fmt.Printf("App RunUntilCancelled failed: %v\n", err)
+		os.Exit(1)
+	}
+}
